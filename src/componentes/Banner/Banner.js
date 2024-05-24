00bd26a85423b77f6e579efe9d9b919eee72f1ea
weslyramalho <weslyramalho@yahoo.com.br>
2024-05-24 14:26:23 -0300

    ajuste

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+function Banner(){
+    return (
+        <header className='banner'>
+        <img src="/imagens/banner.png" alt="banner principal"/>
+        </header>
+    )
+}
+export default Banner;
